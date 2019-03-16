3b0aca734128814c906847c2e5b6e6d9db96e94d
Iris Natashia <irisnatashia@gmail.com>
2019-03-15 23:33:27 -0400

    Creacion Modelos

--- proyectofinanzas/finanzas/models.py
+++ proyectofinanzas/finanzas/models.py
@@ -5,54 +5,71 @@ from django.conf import settings
 
 
 # Create your models here.
-class TipoTransaccion (models.Model):
-    id = models.AutoField (primary_key=True, help_text= 'ID Tipo de Transaccion')
-    Descripcion= models.CharField (max_length=50, help_text='Descripcion')
-    Activo=models.BooleanField (default=1)
-    Tipo=models.CharField(max_length=1, choices=(('I', 'Ingresos'), ('E', 'Egresos'), ('R', 'Renglones de Egresos'), ('P','Tipos de Pagos')),default='')
-
-    def __str__(self):
-        return str(self.Descripcion)
 
 
-class GestionEgresos (models.Model):
-    id = models.AutoField (primary_key=True, help_text= 'ID Gestion de Egreso')
+class Egreso (models.Model):
+    id = models.AutoField (primary_key=True, help_text= 'ID  de Egreso')
     TipoEgreso=models.CharField (max_length=50, help_text='Tipo de egreso')
-    RenglonEgreso=models.CharField (max_length=50, help_text='Renglon de egreso')
-    TipoPagoxDefecto=models.CharField (max_length=50, help_text='Forma De Pago')
     Descripcion= models.CharField (max_length=50, help_text='Descripcion')
     EstadoActivo=models.BooleanField (default=1)
 
     def __str__(self):
-        return str(self.Descripcion)
+        return str(self.TipoEgreso)
 
-class GestionIngresos (models.Model):
+
+class RenglonEgreso (models.Model):
     id = models.AutoField (primary_key=True, help_text= 'ID Gestion de Ingreso')
-    TipoIngreso=models.CharField (max_length=50, help_text='Tipo de ingreso')
-    Origen=models.CharField (max_length=50, help_text='Institucion/Cliente/Empleador')
+    Nombre=models.CharField (max_length=50, help_text='Tipo de ingreso')
     Descripcion= models.CharField (max_length=50, help_text='Descripcion')
-    EstadoActivo=models.BooleanField (default=1)
-
-    def __str__(self):
-        return str(self.Descripcion)
-
-class GestionUsuarios (models.Model):
-    id = models.AutoField (primary_key=True, help_text= 'ID Usuario')
-    Nombre=models.CharField (max_length=50, help_text='Nombre de Usuario')
-    Cedula=models.CharField (max_length=50, help_text='Cedula')
-    Limite= models.CharField (max_length=50, help_text='Limite de Egresos del Periodo')
-    TipoPersona=models.CharField(max_length=1, choices=(('F', 'Fisica'), ('J', 'Juridica')),default='')
-    FechaCorte=models.DateField
+    Institucion=modles.CharField(max_length=35, help_text='Institucion, empleador o cliente')
     EstadoActivo=models.BooleanField (default=1)
 
     def __str__(self):
         return str(self.Nombre)
 
+class TipoPago (models.Model):
+    id = models.AutoField (primary_key=True, help_text= 'ID Gestion de Ingreso')
+    Nombre=models.CharField (max_length=50, help_text='Tipo de de pago (Tarjeta, efectivo, etc).')
+    EstadoActivo=models.BooleanField (default=1)
+
+    def __str__(self):
+        return str(self.Nombre)
+
+class Fuente (models.Model):
+    id = models.AutoField (primary_key=True, help_text= 'ID Gestion de Ingreso')
+    Nombre=models.CharField (max_length=50, help_text='Fuente Ingreso')
+    Institucion=models.CharField(max_length=35)
+    EstadoActivo=models.BooleanField (default=1)
+
+    def __str__(self):
+        return str(self.Nombre)
+
+class Ingreso (models.Model):
+    id = models.AutoField (primary_key=True, help_text= 'ID de Ingreso')
+    TipoIngreso=models.CharField (max_length=50, help_text='Tipo de ingreso')
+    Fuente=models.ForeignKey(Fuente)
+    Descripcion= models.CharField (max_length=50, help_text='Descripcion')
+    EstadoActivo=models.BooleanField (default=1)
+
+    def __str__(self):
+        return str(self.TipoIngreso)
+
+class GestionEgreso (models.Model):
+    id = models.AutoField (primary_key=True, help_text= 'ID Gestion de Ingreso')
+    TipoIngreso=models.ForeignKey (TipoEgreso)
+    Renglon=models.ForeignKey (RenglonEgreso)
+    TipoPago=models.ForeignKey (TipoPago)
+    Descripcion= models.CharField (max_length=50, help_text='Descripcion')
+    EstadoActivo=models.BooleanField (default=1)
+
+    def __str__(self):
+        return str(self.Descripcion)
+
+
 class Transacciones (models.Model):
     id = models.AutoField (primary_key=True, help_text= 'ID Transaccion')
-    Usuario=models.ForeignKey (GestionUsuarios, on_delete=None)
+    Usuario=models.ForeignKey (Users, on_delete=None)
     TipoTransaccion=models.CharField(max_length=1, choices=(('I', 'Ingreso'), ('E', 'Egreso')),default='')
-    TipoPago=models.CharField(max_length=1, choices=(('T', 'Tarjeta de Credito'), ('E', 'Efectivo'), ('C', 'Cheque')),default='')
     FechaTransaccion=models.DateField
     FechaRegistro=models.DateField (auto_now=True)
     Monto=models.CharField (max_length=12, help_text='Monto Transaccion')
@@ -64,7 +81,7 @@ class Transacciones (models.Model):
 
 class Corte (models.Model):
     id = models.AutoField (primary_key=True, help_text= 'ID Corte')
-    Usuario=models.ForeignKey (GestionUsuarios, on_delete=None, default='')
+    Usuario=models.ForeignKey (Users, on_delete=None, default='')
     Ano=models.IntegerField (validators=[MinValueValidator(2000)], default=2019, help_text='Ano')
     Mes=models.CharField(max_length=3, choices=(('Ene', 'Enero'), ('Feb', 'Febrero'),('Mar', 'Marzo'), ('Abr', 'Abril'),
     ('May', 'Mayo'), ('Jun', 'Junio'),('Jul', 'Julio'), ('Ago', 'Agosto'),('Sep', 'Septiembre'), ('Oct', 'Octubre'),
@@ -74,13 +91,9 @@ class Corte (models.Model):
     TotalIngresos=models.IntegerField (default=0)
     TotalEgresos=models.IntegerField (default=0)
     BalanceCorte=models.IntegerField (default=0)
+    Comentario=models.CharField(Max_lenght=40)
+    EstadoActivo=models.BooleanField (default=1)
     
 
     def __str__(self):
         return str(self.Mes)
-
-
-#class Users(AbstractUser):
- #   cedula = models.TextField(max_length=11, blank=True)
-  #  location = models.CharField(max_length=30, blank=True)
-   # birth_date = models.DateField(null=True, blank=True)
